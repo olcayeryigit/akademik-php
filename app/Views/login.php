211b64644e96f09a6281da1ba27db7f48b30227c
olcay <olcayeryigit@hotmail.com>
2025-01-28 11:08:39 +0300

    crud-blog

--- app/Views/login.php
+++ app/Views/login.php
@@ -27,7 +27,6 @@
       <!-- Giriş Butonu -->
       <button type="submit" class="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">Giriş Yap</button>
     </form>
-
   </div>
 </div>
 </body>
